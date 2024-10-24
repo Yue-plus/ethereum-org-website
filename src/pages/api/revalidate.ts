66918c6736bf07d2e8374f8a8c995d60f4f9dbbf
Pablo Pettinari <pettinarip@gmail.com>
2024-10-24 20:42:33 +0200

    add logs

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -40,6 +40,7 @@ export default async function handler(
 
     return res.json({ revalidated: true })
   } catch (err) {
+    console.error(err)
     // If there was an error, Next.js will continue
     // to show the last successfully generated page
     return res.status(500).send("Error revalidating")
